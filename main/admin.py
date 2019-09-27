0838003b5c49372ab57194a1e619807b7edc8dcf
zintesar <zintesar@gmail.com>
2019-09-27 23:40:10 +0600

    v 1.0

---
+++ main/admin.py
@@ -0,0 +1,30 @@
+from django.contrib import admin
+from .models import Tutorial
+from tinymce.widgets import TinyMCE
+from django.db import models
+
+# Register your models here.
+
+class TutorialAdmin(admin.ModelAdmin):
+
+    #fields = ["tutorial_title","tutorial_published","tutorial_content"]
+
+    fieldsets = (
+        ("Title/Date", {
+            "fields": (
+                ["tutorial_title","tutorial_published"]
+                
+            ),
+        }),
+        ("Contetn", {
+            "fields": (
+                ["tutorial_content"]  
+            )})   
+    )
+
+    formfield_overrides = {
+        models.TextField:{'widget': TinyMCE()}
+    }
+    
+
+admin.site.register(Tutorial,TutorialAdmin)
---
+++ main/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Tutorial
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib.auth import authenticate
+from django.contrib import messages
+# Create your views here.
+
+
+def homepage(request):
+   # return HttpResponse("boop")
+
+   return render(request=request,
+                 template_name="main/home.html",
+                 context={"tutorials": Tutorial.objects.all}
+                 )
+
+
+def register(request):
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.data.get('username')
+            messages.success(request, f"New account created {username}")
+            login(request, user)
+            messages.info(request, f"You are now logged in as {username}")
+            return redirect("main:homepage")
+        else:
+            for msg in form.errors:
+                #print(form.errors[msg])
+                messages.error(request,f"{msg}:{form.errors[msg]}")
+                #messages.error(request, f"{msg}:{form.error_messages[msg]}")
+
+    form = UserCreationForm
+    return render(request, "main/register.html", context={"form": form})
