7682c13979d7670ac4f53f59ffec731f9ddbb5b1
zintesar <zintesar@gmail.com>
2019-10-06 23:40:38 +0600

    v1.2 login and logout

--- main/forms.py
+++ main/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
-
+#asdasd
     class Meta:
         model = User
         fields = ("username","email","password1","password2")
